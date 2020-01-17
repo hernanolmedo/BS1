21df3b32b2830ab1da1453e7911d2d66955a33cc
ciber <ciber@LAPTOP-0JK7T32O>
2020-01-17 20:43:33 -0300

    Clase Calculator

---
+++ Exercise1/src/exercise1/Exercise1.java
@@ -0,0 +1,30 @@
+package exercise1;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Hernán Olmedo
+ */
+public class Exercise1 {
+    /**
+     * @param args the command line arguments
+     * @return 
+     */
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        String expression="";
+        try{
+            Scanner input = new Scanner(new File("C:\\Users\\ciber_000\\Documents\\NetBeansProjects\\Exercise1\\src\\exercise1\\expresionNumerica.txt"));
+            expression = input.nextLine();
+            input.close();
+            calc.evaluate(expression);
+        }
+        catch(Exception ex){
+            System.out.println(ex.toString());
+        }
+        System.out.println(calc.evaluate(expression));
+    }
+    
+}
